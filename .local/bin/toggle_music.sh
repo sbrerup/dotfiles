7889a164a501d9da2834d48b30601582054263e1
Sebastian Brerup <sbrerup@beamray.org>
2025-11-20 11:29:01 +0100

    update special workspace logic

--- .local/bin/toggle_music.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CLIENT="com.github.th_ch.youtube_music"
-
-if pgrep -f "youtube-music" > /dev/null; then
-    hyprctl dispatch togglespecialworkspace music
-else
-    youtube-music &
-fi
