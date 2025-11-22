65fb307cf3188218ab1c9cdf2490be83cfce1915
Sebastian Brerup <sbrerup@beamray.org>
2025-11-22 22:46:50 +0100

    fixes and add kitty conf

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #/bin/bash
 
-paru -S zsh zsh-autosuggestions zsh-syntax-highlighting fzf
+paru -S code nvim stow git firefox
+paru -S zsh zsh-autosuggestions zsh-syntax-highlighting fzf unzip ttf-jetbrains-mono-nerd noto-fonts
+paru -S --needed aylurs-gtk-shell-git wireplumber libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python upower pacman-contrib power-profiles-daemon gvfs gtksourceview3 libsoup3 grimblast-git wf-recorder-git hyprpicker matugen-bin python-gpustat hyprsunset-git
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+curl -s https://ohmyposh.dev/install.sh | bash -s
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions &
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting &
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting &
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete &
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab &
+
+
+paru -S hyprpaper
+
+paru -S ags-hyprpanel-git
 
 echo "Installation complete"
